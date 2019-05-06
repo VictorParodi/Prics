ff8c27389fef223cf54f1aa3515d1f17a311808f
VictorParodi <victor.parodi.montero@gmail.com>
2019-05-06 00:44:55 -0500

    app1 commit 7: making the first request with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log('FROM APP', term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: 'Client-ID 9d215ae4d114eb29d4f68772730e61c8a2bb27d90cb40e26c48b09c8925f3843'
+            }
+        });
     }
 
     render() {

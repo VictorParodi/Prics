193621afa9c5143c55c8ca21c5d9ff08c15dfdd7
VictorParodi <victor.parodi.montero@gmail.com>
2019-05-11 06:35:14 -0500

    app1 commit 11: using refs system

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', () => {
+            console.log(this.imageRef.current.clientHeight);
+        });
+    }
+
     render() {
         let {description, urls} = this.props.image;
 
         return (
             <div>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.small}
                 />
